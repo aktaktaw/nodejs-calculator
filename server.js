e43cf517c96b59fd25c19488acdad03d9879712e
aktaktaw <aktaktaw@gmail.com>
2020-08-31 01:04:13 +0800

    create calculator layout
    create simple layout calculator

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine','ejs')
+app.use(express.static('./public'))
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('pages/index')
 })
 
 app.listen(port, () => {
